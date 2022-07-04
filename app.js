534337e88b6c3eaa4249c2a438d0db9d2b39d7f2
Сергей Батурин <94468513+BaturinSS@users.noreply.github.com>
2022-07-05 00:23:48 +0300

    completed users api

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ const mongoose = require('mongoose');
 //* Подключаем модуль обработки запроса body
 const bodyParser = require('body-parser');
 
-//* Возьмём порт (по умолчанию 3000) и базовый URL из переменной окружения
-const { PORT = 3000, BASE_PATH } = process.env;
+//* Возьмём порт (по умолчанию 3000) из переменной окружения
+const { PORT = 3000 } = process.env;
+
+//* Импортировать модуль users
+const usersRouter = require('./routes/users');
 
 //* Создаем приложение методом express
 const app = express();
@@ -18,15 +21,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //* Подключаемся к серверу mongo
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+//* Перенаправляем запрос на нужный адрес
+app.use('/users', usersRouter);
 
 //* Установим слушателя на порт
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
-  console.log('Ссылка на сервер:');
-  console.log(BASE_PATH);
 });
--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,41 @@
 //* Oбъект связанный с app, на который мы и повесим обработчики
 const router = require('express').Router();
 
+//* Импорт модели данных
+const User = require('../models/user');
+
 //* Принимаем запрос GET
-router.get('/', (req, res) => {
-  res.send(`Пользователь ${name}, ${age} лет`);
-});
-
-router.get('/:id', (req, res) => {
-  if (!users[req.params.id]) {
-    res.send('Такого пользователя не существует');
-    return;
-  }
-
-  const { name, age } = users[req.params.id];
-
-  res.send(`Пользователь ${name}, ${age} лет`);
-});
+router
+  .get('/', (req, res) => {
+    User
+      .find({})
+      .then((users) => {
+        res
+          .status(200)
+          .send(users);
+      })
+      .catch((err) => console.error(err));
+  })
+  .get('/:id', (req, res) => {
+    User
+      .findById(req.params.id)
+      .then((user) => {
+        res
+          .status(200)
+          .send(user);
+      })
+      .catch((err) => console.error(err));
+  })
+  .post('/', (req, res) => {
+    User
+      .create(req.body)
+      .then((user) => {
+        res
+          .status(201)
+          .send(user);
+      })
+      .catch((err) => console.error(err));
+  });
 
 //* Экспортировали роутер
 module.exports = router;
