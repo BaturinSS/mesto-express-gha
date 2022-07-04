435ca90feb56a6d65a4944183e53df48a0439949
Сергей Батурин <94468513+BaturinSS@users.noreply.github.com>
2022-07-04 17:29:42 +0300

    ++

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 //* Подключаем фреймворк express для сервера на ноде
 const express = require('express');
 
+//* Подключаем модуль для работы с базой данных в MongoDB
+const mongoose = require('mongoose');
+
 //* Настроим порт, который должен слушать приложение
 //* Возьмём его из переменной окружения
 //* По умолчание установим порт 3000
@@ -8,6 +11,13 @@ const { PORT = 3000 } = process.env;
 
 //* Создаем приложение методом express
 const app = express();
+
+//* Подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 //* Установим слушателя на порт
 app.listen(PORT, () => {
