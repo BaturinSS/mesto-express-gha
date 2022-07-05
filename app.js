deb5ee557d8016eabc908b36edcaf3dc666913af
Сергей Батурин <94468513+BaturinSS@users.noreply.github.com>
2022-07-05 18:53:58 +0300

    ++

--- app.js
+++ app.js
@@ -33,6 +33,11 @@ app.use((req, res, next) => {
 });
 app.use('/users', usersRouter);
 app.use('/cards', cardsRouter);
+app.use('/', (req, res) => {
+  res
+    .status(404)
+    .send({ message: 'Страница не существет' });
+});
 
 //* Установим слушателя на порт
 app.listen(PORT, () => {
