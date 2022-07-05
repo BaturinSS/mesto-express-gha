ad72e5b107b1ebcef80443d504b9cc6f6f42c3fb
Сергей Батурин <94468513+BaturinSS@users.noreply.github.com>
2022-07-05 17:55:03 +0300

    completed error user api

--- controllers/users.js
+++ controllers/users.js
@@ -10,17 +10,90 @@ module.exports.getUsers = (req, res) => {
         .status(200)
         .send(users);
     })
-    .catch((err) => console.error(err));
+    .catch((err) => {
+      res
+        .status(500)
+        .send({
+          message: `${err.name}: ${err.message}`,
+        });
+    });
 };
 module.exports.getUser = (req, res) => {
   User
     .findById(req.params.id)
+    .orFail(new Error('Такого пользователя нет'))
     .then((user) => {
       res
         .status(200)
         .send(user);
     })
-    .catch((err) => console.error(err));
+    .catch((err) => {
+      if (err.name === 'Error') {
+        res
+          .status(404)
+          .send({ message: `${err.name}: ${err.message}` });
+      } else {
+        res
+          .status(500)
+          .send({ message: `${err.name}: ${err.message}` });
+      }
+    });
+};
+module.exports.updateUser = (req, res) => {
+  User
+    .findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    })
+    .orFail(new Error('Такого пользователя нет'))
+    .then((user) => {
+      res
+        .status(201)
+        .send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res
+          .status(400)
+          .send({ message: `${err.name}: ${err.message}` });
+      } if (err.name === 'Error') {
+        res
+          .status(404)
+          .send({ message: `${err.name}: ${err.message}` });
+      } else {
+        res
+          .status(500)
+          .send({ message: `${err.name}: ${err.message}` });
+      }
+    });
+};
+module.exports.updateUserAvatar = (req, res) => {
+  User
+    .findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    })
+    .orFail(new Error('Такого пользователя нет'))
+    .then((user) => {
+      res
+        .status(201)
+        .send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res
+          .status(400)
+          .send({ message: `${err.name}: ${err.message}` });
+      } if (err.name === 'Error') {
+        res
+          .status(404)
+          .send({ message: `${err.name}: ${err.message}` });
+      } else {
+        res
+          .status(500)
+          .send({ message: `${err.name}: ${err.message}` });
+      }
+    });
 };
 module.exports.createUser = (req, res) => {
   User
@@ -30,25 +103,15 @@ module.exports.createUser = (req, res) => {
         .status(201)
         .send(user);
     })
-    .catch((err) => console.error(err));
-};
-module.exports.updateUser = (req, res) => {
-  User
-    .findByIdAndUpdate(req.body)
-    .then((user) => {
-      res
-        .status(201)
-        .send(user);
-    })
-    .catch((err) => console.error(err));
-};
-module.exports.updateUserAvatar = (req, res) => {
-  User
-    .create(req.body)
-    .then((user) => {
-      res
-        .status(201)
-        .send(user);
-    })
-    .catch((err) => console.error(err));
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res
+          .status(400)
+          .send({ message: `${err.name}: ${err.message}` });
+      } else {
+        res
+          .status(500)
+          .send({ message: `${err.name}: ${err.message}` });
+      }
+    });
 };
